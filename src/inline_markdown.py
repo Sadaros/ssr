de87737c36f329adc340279c8bee797fa7c06afc
Sadaros <post@chrislearns.tech>
2024-03-06 23:24:31 +0100

    finished inline_markdown.py and corresponding tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -12,17 +12,22 @@ text_types: list[str] = ["text", "bold", "italic", "code", "link", "image",]
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: str,) -> list[TextNode]:
     new_nodes = []
     for node in old_nodes:
-#        print(node)
-        if not isinstance(node, TextNode):
+        if node.text_type != text_type_text:
             new_nodes.append(node)
             continue
+
         split_text = node.text.split(delimiter)
+        split_nodes = []
         if len(split_text) % 2 == 0:
             raise ValueError(f"Mismatched delimiters in node: {node}")
 
         for i,w in enumerate(split_text):
+            if w == "":
+                continue
             if i % 2 == 0:
-                new_nodes.append(TextNode(text=w, text_type="text"))
+                split_nodes.append(TextNode(text=w, text_type=text_type_text))
             else:
-                new_nodes.append(TextNode(text=w, text_type=text_type))
+                split_nodes.append(TextNode(text=w, text_type=text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
+
